09a3be04f89c749648941ec0d909f5db67fee0bd
Alexander Shibaew <sasha.schibaew@yandex.ru>
2025-06-19 23:35:56 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      {count > 5 && (<h1>Vite + Reactors </h1>)}
+       <h1>Vite + Reactors </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
